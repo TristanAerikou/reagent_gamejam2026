825362329251d5c8079a7ca4c010537013cfc09a
Wolf Bruyland <wolf.bruyland@student.hogent.be>
2026-04-11 00:11:24 +0200

    player movement and animation

--- reagent-26-gamejam-project/scripts/character_body_2d.gd
+++ reagent-26-gamejam-project/scripts/character_body_2d.gd
@@ -1,11 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 75
+@export var speed = 75
 
-func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	velocity = direction * SPEED
 
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+	return input_direction
+
+func change_sprite(direction):
+	var walking_left = Vector2(-1.0,0.0)
+	var walking_right = Vector2(1.0,0.0)
+	var walking_up = Vector2(0.0,-1.0)
+	var walking_down = Vector2(0.0,1.0)
+	var idle = Vector2(0.0,0.0)
+	match direction:
+		idle:
+			match $AnimatedSprite2D.animation:
+				"player_walking_left":
+					$AnimatedSprite2D.animation = "player_idle_left"
+				"player_walking_right":
+					$AnimatedSprite2D.animation = "player_idle_right"
+				"player_walking_up":
+					$AnimatedSprite2D.animation = "player_idle_up"
+				"player_walking_down":
+					$AnimatedSprite2D.animation = "player_idle_down"
+		walking_left:
+			$AnimatedSprite2D.animation = "player_walking_left"
+		walking_right:
+			$AnimatedSprite2D.animation = "player_walking_right"
+		walking_up:
+			$AnimatedSprite2D.animation = "player_walking_up"
+		walking_down:
+			$AnimatedSprite2D.animation = "player_walking_down"
+		
+func _physics_process(delta):
+	change_sprite(get_input())
 	move_and_slide()
