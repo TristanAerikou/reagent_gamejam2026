114de2ee0ae63454b778892aa603ce79cdb53030
four-griffins <four.griffins.dev@gmail.com>
2026-04-11 12:49:18 +0200

    kasten met verschillende item sprites

--- reagent-26-gamejam-project/scripts/kast_script.gd
+++ reagent-26-gamejam-project/scripts/kast_script.gd
@@ -1,11 +1,7 @@
 extends StaticBody2D
-var item_type = ""
 
-# Called when the node enters the scene tree for the first time.
+@export var item_type: mainScript.Ingredient
+@export var item: Texture2D
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$Item_Sprite.texture = item
--- reagent-26-gamejam-project/scripts/mainScript.gd
+++ reagent-26-gamejam-project/scripts/mainScript.gd
@@ -1,5 +1,7 @@
 extends Node2D
+class_name mainScript
 
+enum Ingredient {SOEP, WIJN, ZOUT}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
