382f1df631abf8c26009015c515c101166c5b96e
four-griffins <four.griffins.dev@gmail.com>
2026-04-11 22:11:41 +0200

    add items en kasten vanuit editor

--- reagent-26-gamejam-project/scripts/kast_script.gd
+++ reagent-26-gamejam-project/scripts/kast_script.gd
@@ -2,11 +2,9 @@ extends StaticBody2D
 
 @export var item_type: mainScript.Ingredient
 @export var item_texture: Texture2D
-@export var item_texture_rect : Rect2
 
 @export var opberg_type : mainScript.Opberg
 @export var opberg_texture: Texture2D
-@export var opberg_texture_rect : Rect2
 
 func _ready() -> void:
 	
@@ -21,9 +19,4 @@ func _ready() -> void:
 	
 	
 	$Item_Sprite.texture = item_texture
-	$Item_Sprite.region_enabled = true
-	$Item_Sprite.region_rect = item_texture_rect
-	
 	$Kast_Sprite.texture = opberg_texture
-	$Kast_Sprite.region_enabled = true
-	$Kast_Sprite.region_rect = opberg_texture_rect
--- reagent-26-gamejam-project/scripts/mainScript.gd
+++ reagent-26-gamejam-project/scripts/mainScript.gd
@@ -4,55 +4,33 @@ class_name mainScript
 enum Ingredient {ZOUT, BOT, GOUDVIS, KOEKJE, KAAS}
 enum Opberg {KAST, TAFEL, KLEINKASTJE}
 
-@onready var plaatsen = [
-	Vector2i(112, -48),
-	Vector2i(80, -48),
-	Vector2i(48, -48),
-	]
-@onready var opberg_textures = [
-	[Opberg.KAST, $Item_Opberg/Textures/Kast], 
-	[Opberg.TAFEL, $Item_Opberg/Textures/Tafel],
-	[Opberg.KLEINKASTJE, $Item_Opberg/Textures/KleinKastje]
-]
-@onready var item_textures = [
-	$Item_Opberg/Items/Zout,
-	$Item_Opberg/Items/Bot,
-	$Item_Opberg/Items/Goudvis,
-	$Item_Opberg/Items/Koekje,
-	$Item_Opberg/Items/Kaas
-]
+const kast_template = preload("res://scenes/kast.tscn")
+
+@export var plaatsen: Array[Vector2i]
+@export var opberg_textures: Dictionary[Opberg, AtlasTexture]
+@export var item_textures: Dictionary[Ingredient, AtlasTexture]
 
 func _ready() -> void:
 	# BELANGRIJK:
 	# Als we willen potions maken, gaan we moeten bijhouden welke items er effectief in het spel zitten.
 	# Als je de code van plaatsOpbergRuimtes() doorleest, zie je dat er uit de originele lijst items worden verwijderd.
 	# Fix: steek de gebruikte items in een lijst die ook globaal wordt bijgehouden.
-	
-	randomize()
-	plaatsOpbergRuimtes(plaatsen, opberg_textures, item_textures)
-		
-func plaatsOpbergRuimtes(posities : Array, opbergTextures : Array, itemTextures : Array) -> void:
-	for p in posities:
-		# random
-		var opbergType = randi_range(0, opbergTextures.size()-1)
-		var itemType = randi_range(0, itemTextures.size()-1)
+	for p in plaatsen:
 		
 		# nieuwe kast aanmaken
-		var nieuweKast = $KastTemplate.duplicate()
-		nieuweKast.position = p
+		var nieuwe_kast = kast_template.instantiate()
+		nieuwe_kast.position = p
+		
+		# random
+		var opberg_type = randi_range(0, len(Opberg)-1)
+		var item_type = randi_range(0, len(Ingredient)-1)
 		
 		# opbergruimte texture setten
-		nieuweKast.opberg_type = opbergTextures[opbergType][0]
-		nieuweKast.opberg_texture = opbergTextures[opbergType][1].texture
-		nieuweKast.opberg_texture_rect = opbergTextures[opbergType][1].region_rect
+		nieuwe_kast.opberg_type = opberg_type
+		nieuwe_kast.opberg_texture = opberg_textures.get(opberg_type)
 		
 		# item texture setten
-		nieuweKast.item_texture = itemTextures[itemType].texture
-		nieuweKast.item_texture_rect = itemTextures[itemType].region_rect
+		nieuwe_kast.item_type = item_type
+		nieuwe_kast.item_texture = item_textures.get(item_type)
 		
-		itemTextures.remove_at(itemType)
-		add_child(nieuweKast)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta: float) -> void:
-# 	pass
+		add_child(nieuwe_kast)
