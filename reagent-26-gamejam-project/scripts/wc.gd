35410a55e3f41b40fe838a30c6b8765a99c0e572
four-griffins <four.griffins.dev@gmail.com>
2026-04-15 12:16:12 +0200

    orders checken en vervullen

--- reagent-26-gamejam-project/scripts/wc.gd
+++ reagent-26-gamejam-project/scripts/wc.gd
@@ -1,6 +1,17 @@
 extends Node
 
 const types = preload("res://scripts/global_data.gd")
+@onready var orders: HBoxContainer = $"../Camera2D/Orders"
 
 func submit_potion(potion: Array[types.Ingredient]):
-	print("drankje ingediend", potion)
+	for order in orders.get_children():
+		var matching := true
+		for item in types.Ingredient.values():
+			print("comparign ", item)
+			if potion.count(item) != order.ingredients.count(item):
+				matching = false
+				break
+		
+		if matching:
+			order.queue_free()
+			print("fulfilled order!")
