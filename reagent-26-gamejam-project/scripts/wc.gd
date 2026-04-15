66c9593bab563dd7e6e4a002f1d0bf582024c9ff
four-griffins <four.griffins.dev@gmail.com>
2026-04-15 12:20:26 +0200

    shitty score label

--- reagent-26-gamejam-project/scripts/wc.gd
+++ reagent-26-gamejam-project/scripts/wc.gd
@@ -2,16 +2,17 @@ extends Node
 
 const types = preload("res://scripts/global_data.gd")
 @onready var orders: HBoxContainer = $"../Camera2D/Orders"
+var score: int = 0
 
 func submit_potion(potion: Array[types.Ingredient]):
 	for order in orders.get_children():
 		var matching := true
 		for item in types.Ingredient.values():
-			print("comparign ", item)
 			if potion.count(item) != order.ingredients.count(item):
 				matching = false
 				break
 		
 		if matching:
 			order.queue_free()
-			print("fulfilled order!")
+			score += 10
+			$"../ScoreLabel".text = "Score: " + str(score)
