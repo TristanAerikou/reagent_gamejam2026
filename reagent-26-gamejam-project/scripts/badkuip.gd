2b2531dc2a748cc0dac246f2e728112a909fa1d0
four-griffins <four.griffins.dev@gmail.com>
2026-04-16 12:34:31 +0200

    water in badkuip

--- reagent-26-gamejam-project/scripts/badkuip.gd
+++ reagent-26-gamejam-project/scripts/badkuip.gd
@@ -4,6 +4,9 @@ class_name Badkuip
 const enums = preload("res://scripts/global_data.gd")
 const global_data = preload("res://scripts/global_data.tres")
 
+@export var leeg_texture: AtlasTexture
+@export var vol_texture: AtlasTexture
+
 var ingredients: Array[enums.Ingredient] = []
 
 func _on_body_entered(body: Node2D):
@@ -17,10 +20,14 @@ func add_ingredient(ingredient: enums.Ingredient):
 	var tex := TextureRect.new()
 	tex.texture = global_data.item_textures[ingredient]
 	%BadItems.add_child(tex)
+	
+	if ingredient == enums.Ingredient.WATER:
+		$BadkuipSprite.texture = vol_texture
 
 func clear_badkuip() -> Array[enums.Ingredient]:
 	var copy := ingredients.duplicate()
 	ingredients.clear()
 	for child in %BadItems.get_children():
 		child.queue_free()
+	$BadkuipSprite.texture = leeg_texture
 	return copy
